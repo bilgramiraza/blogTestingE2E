9c50fb512abbd84360399423084531d6a3bf977d
Raza <bilgramiraza@yahoo.com>
2024-09-02 11:29:35 +0530

    Added Tests for Login

--- tests/blog_app.spec.js
+++ tests/blog_app.spec.js
@@ -21,5 +21,39 @@ describe('Blog App', () => {
 
     await expect(page.getByRole('button', { name: 'Login' })).toBeVisible();
     await expect(page.getByRole('button', { name: 'Cancel' })).toBeVisible();
-  });
+  })
+  describe('Login', async () => {
+    beforeEach(async ({ page, request }) => {
+      await request.post('/api/testing/reset');
+      await request.post('/api/users', {
+        data: {
+          username: 'test',
+          name: 'tester',
+          password: 'secret',
+        }
+      });
+    })
+    test('Succeeds with valid Credentials', async ({ page }) => {
+      await page.getByRole('button', { name: 'Login' }).click();
+
+      await page.getByTestId('username').fill('test');
+      await page.getByTestId('password').fill('secret');
+
+      await page.getByTestId('login').click();
+
+      await expect(page.getByText('tester has Logged In')).toBeVisible();
+      await expect(page.getByText('tester has Logged In')).toHaveCSS('border-color', 'rgb(0, 128, 0)');
+    })
+    test('Fails with invalid Credentials', async ({ page }) => {
+      await page.getByRole('button', { name: 'Login' }).click();
+
+      await page.getByTestId('username').fill('test');
+      await page.getByTestId('password').fill('secre');
+
+      await page.getByTestId('login').click();
+
+      await expect(page.getByText('invalid username or password')).toBeVisible();
+      await expect(page.getByText('invalid username or password')).toHaveCSS('border-color', 'rgb(255, 0, 0)');
+    })
+  })
 });
