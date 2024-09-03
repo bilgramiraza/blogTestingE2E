13b9fe457368ee3ec7153905ef1d2791ed381ac3
Raza <bilgramiraza@yahoo.com>
2024-09-03 17:24:16 +0530

    Added Helper funcitons for tests

--- tests/helper.js
+++ tests/helper.js
@@ -7,6 +7,17 @@ const loginWith = async (page, username, password) => {
   await page.getByTestId('login').click();
 };
 
+const createBlog = async (page, title, author, url) => {
+  await page.getByRole('button', { name: 'Create New Blog' }).click();
+
+  await page.getByTestId('title').fill(title);
+  await page.getByTestId('author').fill(author);
+  await page.getByTestId('url').fill(url);
+
+  await page.getByTestId('create').click();
+};
+
 export {
   loginWith,
+  createBlog,
 };
