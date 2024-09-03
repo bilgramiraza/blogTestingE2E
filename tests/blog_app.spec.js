e3763671fd5431837b7499a6e7bf247a5bfb7e10
Raza <bilgramiraza@yahoo.com>
2024-09-03 17:25:12 +0530

    Added Test for Adding New Blogs and Implemented the Helper Funcitons

--- tests/blog_app.spec.js
+++ tests/blog_app.spec.js
@@ -1,8 +1,16 @@
 const { test, describe, beforeEach, expect } = require("@playwright/test");
-const { loginWith } = require("./helper");
+const { loginWith, createBlog } = require("./helper");
 
 describe('Blog App', () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('/api/testing/reset');
+    await request.post('/api/users', {
+      data: {
+        username: 'test',
+        name: 'tester',
+        password: 'secret',
+      }
+    });
     await page.goto('/');
   })
   test('Front Page Loads', async ({ page }) => {
@@ -24,27 +32,38 @@ describe('Blog App', () => {
     await expect(page.getByRole('button', { name: 'Cancel' })).toBeVisible();
   })
   describe('Login', async () => {
-    beforeEach(async ({ page, request }) => {
-      await request.post('/api/testing/reset');
-      await request.post('/api/users', {
-        data: {
-          username: 'test',
-          name: 'tester',
-          password: 'secret',
-        }
-      });
-    })
     test('Succeeds with valid Credentials', async ({ page }) => {
+      const notificationDiv = page.getByTestId('notification');
+
       await loginWith(page, 'test', 'secret');
 
-      await expect(page.getByText('tester has Logged In')).toBeVisible();
-      await expect(page.getByText('tester has Logged In')).toHaveCSS('border-color', 'rgb(0, 128, 0)');
+      await expect(notificationDiv.getByText('tester has Logged In')).toBeVisible();
+      await expect(notificationDiv.getByText('tester has Logged In')).toHaveCSS('border-color', 'rgb(0, 128, 0)');
     })
     test('Fails with invalid Credentials', async ({ page }) => {
+      const notificationDiv = page.getByTestId('notification');
+
       await loginWith(page, 'test', 'secre');
 
-      await expect(page.getByText('invalid username or password')).toBeVisible();
-      await expect(page.getByText('invalid username or password')).toHaveCSS('border-color', 'rgb(255, 0, 0)');
+      await expect(notificationDiv.getByText('invalid username or password')).toBeVisible();
+      await expect(notificationDiv.getByText('invalid username or password')).toHaveCSS('border-color', 'rgb(255, 0, 0)');
+    })
+  })
+  describe('When logged in', async () => {
+    beforeEach(async ({ page }) => {
+      await loginWith(page, 'test', 'secret');
+    })
+    test('A new blog can be created', async ({ page }) => {
+      const notificationDiv = page.getByTestId('notification');
+      const blogListDiv = page.getByTestId('bloglist');
+
+      await createBlog(page, 'First Blog', 'tester', 'tester.com');
+
+      await expect(notificationDiv.getByText('Blog(First Blog) Created Successfully')).toBeVisible();
+      await expect(notificationDiv.getByText('Blog(First Blog) Created Successfully')).toHaveCSS('border-color', 'rgb(0, 128, 0)');
+
+      await expect(blogListDiv.getByText('First Blog')).toBeVisible();
+      await expect(blogListDiv).toHaveCount(1);
     })
   })
 });
